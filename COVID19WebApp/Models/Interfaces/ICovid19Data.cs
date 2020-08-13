bd2859c264861a7a4c2565fab1482d443107a16b
Sue Tarazi <suemachtley@gmail.com>
2020-08-13 12:57:05 -0700

    Add project files.

---
+++ COVID19WebApp/Models/Interfaces/ICovid19Data.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COVID19WebApp.Models
+{
+    interface ICovid19Data
+    {
+    }
+}
