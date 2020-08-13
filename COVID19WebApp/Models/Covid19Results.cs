680656dcb941b92a6c1916824c419931be91e7aa
Sue Tarazi <suemachtley@gmail.com>
2020-08-13 13:39:25 -0700

    model created

---
+++ COVID19WebApp/Models/Covid19Results.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace COVID19WebApp.Models
+{
+    public class Covid19Results
+    {
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("country_abbreviation")]
+        public string CountryAbbreviation { get; set; }
+
+        [JsonPropertyName("total_cases")]
+        public string TotalCases { get; set; }
+
+        [JsonPropertyName("new_cases")]
+        public string NewCases { get; set; }
+
+        [JsonPropertyName("total_deaths")]
+        public string TotalDeaths { get; set; }
+
+        [JsonPropertyName("new_deaths")]
+        public string NewDeaths { get; set; }
+
+        [JsonPropertyName("total_recovered")]
+        public string TotalRecovered { get; set; }
+
+        [JsonPropertyName("active_cases")]
+        public string ActiveCases { get; set; }
+
+        [JsonPropertyName("serious_critical")]
+        public string SeriousCritical { get; set; }
+
+        [JsonPropertyName("cases_per_mill_pop")]
+        public string CasesPerMillionPopulation { get; set; }
+
+        [JsonPropertyName("flag")]
+        public string Flag { get; set; }
+        
+        
+    }
+}
---
+++ COVID19WebApp/Models/DataObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace COVID19WebApp.Models
+{
+    public class DataObject
+    {
+        [JsonPropertyName("data")]
+        public RowsObject Data { get; set; }
+    }
+}
