284c22c6bdd7906edf0d4ae9e124ddb1abc7ac04
Sue Tarazi <suemachtley@gmail.com>
2020-09-17 11:10:24 -0700

    notes from Amanda

--- COVID19WebApp/Views/Home/Index.cshtml.cs
+++ COVID19WebApp/Views/Home/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using COVID19WebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,18 +10,23 @@ namespace COVID19WebApp.Views.Home
 {
     public class IndexModel : PageModel
     {
+        //dependency injection
 
+        public WorldAndCountryViewModel AllData { get; set; }
 
         public void OnGet()
         {
-
+            WorldDataObject result = await _covid19.GetCovid19WorldData();
 
         }
+        
 
         public void OnPost()
         {
 
-
+            List<CountryResults> results = await _covid19.GetCovid19DataForCountry(country);
+            return RedirectToAction(Action, Controller, country);
+        
         }
     }
 }
